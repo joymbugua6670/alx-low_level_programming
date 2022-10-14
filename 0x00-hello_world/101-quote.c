d38b231fcebcb51c16ba1e2d47673baf134e18c5
joymbugua6670 <joymbugua6670>
2022-10-14 15:53:35 -0700

    uou

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio>
+#include <unistd.h>
+/**
+ * main - Entry
+ * Return: 1
+ */
+int  main(void)
+{
+	write(1, "and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19, ");
+	return (1);
+}
+
