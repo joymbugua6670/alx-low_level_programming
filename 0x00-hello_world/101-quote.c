54e6163b1209df898060b27a5570f2b6fca496e9
joymbugua6670 <joymbugua6670>
2022-10-14 16:51:28 -0700

    wue

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 /**
  * main - Entry
- * Return: 1
+ * Return: 0
  */
 int  main(void)
 {
-	(1, "and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19, \n", 59);
+	write(1, "and that piece of art is useful" - Dora Korpar, 2015-10-19,\n", 59);
 	return (1);
 }
 
