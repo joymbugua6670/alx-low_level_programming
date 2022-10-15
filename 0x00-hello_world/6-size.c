d49fe0218687e173dd0ea586ea91eb9e4efdc450
joymbugua6670 <joymbugua6670>
2022-10-14 17:27:01 -0700

    success

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #incude < stdio.h >
 /**
  * main - Entry
@@ -13,28 +12,4 @@ int main(void)
 	    printf("Size of a float: %i byte(s)\n", sizeof(float));
 	return (0);
 }
-=======
-#include <stdio.h>
-/**
- * main - C program that prints the size of various types on the computer
- *
- * Return: (0)
- */
-int main(void)
-{
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
 
-	printf("Size of char: %lu byte(S)\n", (unsigned long)sizeof(a));
-        printf("Size of int: %lu byte(S)\n", (unsigned long)sizeof(b));
-	printf("Size of long int: %lu byte(S)\n", (unsigned long)sizeof(c));
-	printf("Size long long int: %lu byte(S)\n", (unsigned long)sizeof(d));
-	printf("Size of float: %lu byte(S)\n", (unsigned long)sizeof(e));
-	return (o);
-}
- 
-
->>>>>>> 078a4ccb1578fddbd26e8d93cb0a26c7f7d1b87b
