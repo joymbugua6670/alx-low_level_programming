0558a9cd12a246af451083f574d40a42c8415802
joymbugua6670 <joymbugua6670>
2022-10-16 13:33:43 -0700

    pendant

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabets in lowercase
+ * followed by a new line, except q and e
+ * Return: 0
+ */
+int main(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+	{
+		if (la != 'e' && la != 'q')
+			putchar(la);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
