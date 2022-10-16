2781f7752fb004e6fab356d9b751fe4a2740b526
joymbugua6670 <joymbugua6670>
2022-10-16 14:52:37 -0700

    fath

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main -entry
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	char c;
+
+	int d;
+
+	c = 'a';
+	while
+		(d < 10) {
+			putchar(d + '0');
+			d++;
+		}
+	while
+		(c <= 'f') {
+			putchar(c);
+			c++;
+		}
+	return (0);
+}
