c06c37e1f73b568aa2e38c4beee50ed9cdef28bb
joymbugua6670 <joymbugua6670>
2022-10-16 12:34:45 -0700

    dark

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio>
+
+/**
+ * main - print in lower case
+ * Return: 0
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+	}
+	putchar("\n");
+	return (0);
+}
