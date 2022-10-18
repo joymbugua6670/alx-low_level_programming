3ac7a5498ea910dca88e05d5e5d043726b166556
joymbugua6670 <joymbugua6670>
2022-10-18 13:06:21 -0700

    hanger

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - compute absolute numbera for intergers
+ * @i: any interger
+ * Return: Absolute number
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+		return (i);
+	else
+		return (-i);
+}
