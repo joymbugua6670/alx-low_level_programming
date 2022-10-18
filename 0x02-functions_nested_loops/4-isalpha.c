2c911da961026c2f35162fad6148c5012c19c829
joymbugua6670 <joymbugua6670>
2022-10-18 12:31:00 -0700

    hush

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - check whetherv lower or uppercase
+ * @c: is character
+ * Return: 1 if lower or uppercase else return 0
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
