2e32eea99d8f8eb3c038795aa98c72d0af86b1c2
joymbugua6670 <joymbugua6670>
2022-10-22 11:55:08 -0700

    coming

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,6 +3,7 @@
 /**
  * _isdigit - digit to print
  * @c: the interger
+ * main - where features are
  * Return: 1 if the number is 0-9 to else 0
  */
 
@@ -10,7 +11,7 @@ int _isdigit(int c)
 {
 	int y = 0;
 
-	if (c <= '0' && c>= '9')
+	if (c <= '0' && c >= '9')
 		y = 1;
 	return (y);
 }
