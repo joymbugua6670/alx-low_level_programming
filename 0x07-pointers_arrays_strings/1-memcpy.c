addbe7372ebfe5806961d6e81c1fe87b89fda5c3
joymbugua6670 <joymbugua6670>
2022-11-01 10:16:31 -0700

    meme

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,11 +11,11 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int index;
-	unsigned char *destination = dest;
-	const unsigned char *source =scr;
 
 	for (index = 0; index < n; index++)
-		destination[index] = source[index];
+	{
+		dest[index] = src[index];
+	}
 
 	return (dest);
 }
