47ccd3350ebdca41aacfc0891c104755d3d5fb62
joymbugua6670 <joymbugua19@gmail.com>
2022-11-04 01:48:27 -0700

    prime-numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,7 +10,7 @@
 
 int _isitprime(int n, int i)
 {
-	if (n == 1)
+	if (i == 1)
 	{
 		return (1);
 	}
@@ -32,7 +32,7 @@ int _isitprime(int n, int i)
 int is_prime_number(int n)
 {
 	if (n < 2)
-		return (1);
+		return (0);
 	if (n % 2 == 0)
 		return (0);
 	else
